3ea538a3006328bc0108bb12014ce72e3757fc8b
zach86678 <160188932+zach86678@users.noreply.github.com>
2024-02-16 11:43:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice):
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM fruit_load_list")
-    return my_cur.fethall()
+    return my_cur.fetchall()
   
 
 streamlit.title('My Mom\'s New Healthy Dinner')
